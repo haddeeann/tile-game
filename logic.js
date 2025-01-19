f02b27bd06ea9f373bb6050cedf60d9f36713a80
HaddeeAndd <haddeeann@gmail.com>
2025-01-19 16:33:26 -0500

    add all the tiles when the user clicks the button

--- logic.js
+++ logic.js
@@ -1,9 +1,25 @@
 const dealTilesButton = document.getElementById('dealTilesButton');
 dealTilesButton.addEventListener('click', dealTiles);
 
-const tileCircles = document.getElementById('tileCircles')
+const tileCircles = document.getElementById('tileCircles');
 export function dealTiles () {
-    console.log('deal tiles')
+    /* color of tiles */
+    const tileColors = ['dark-gray', 'dark-tan', 'dark-pink', 'dark-blue', 'dark-yellow']
+    /* circle */
+    for (let i = 0; i < 5; i++) {
+        const circle = document.createElement('div');
+        circle.classList.add('circle');
+        /* tile */
+        const tile = document.createElement('div');
+        tile.classList.add('tile');
+        for (let tileColor of tileColors) {
+            let colorTile = tile.cloneNode(false);
+            colorTile.classList.add(tileColor)
+            circle.appendChild(colorTile);
+        }
+
+        tileCircles.appendChild(circle);
+    }
 }
 
 export function shuffleTiles () {
