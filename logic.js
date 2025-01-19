026797f5ae2c94e2bb8165d71255d794ffd22378
HaddeeAndd <haddeeann@gmail.com>
2025-01-19 16:11:43 -0500

    should use camel case, its js
    re-organize a bit, add some skeleton js functions

---
+++ logic.js
@@ -0,0 +1,11 @@
+const dealTilesButton = document.getElementById('dealTilesButton');
+dealTilesButton.addEventListener('click', dealTiles);
+
+const tileCircles = document.getElementById('tileCircles')
+export function dealTiles () {
+    console.log('deal tiles')
+}
+
+export function shuffleTiles () {
+    console.log('shuffle tiles')
+}
