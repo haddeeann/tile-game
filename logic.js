f3adc1a8b646d6905fa66f01fe9fa9c83b1bdc5c
HaddeeAndd <haddeeann@gmail.com>
2025-01-26 13:19:48 -0500

    remove tiles from the circles

--- logic.js
+++ logic.js
@@ -1,6 +1,18 @@
 const dealTilesButton = document.getElementById('dealTilesButton');
 dealTilesButton.addEventListener('click', dealTiles);
-
+function handleMoveTile(event) {
+    const clickedEl = event.target;
+    const clickedTile = clickedEl.closest('.tile');
+    const tileClassToRemove = Array.from(clickedTile.classList).find(className => className !== 'tile');
+    const parentCircle = clickedTile.closest('.circle');
+    if (tileClassToRemove) {
+        Array.from(parentCircle.children).forEach(child => {
+            if (child.classList.contains(tileClassToRemove)) {
+                child.remove();
+            }
+        })
+    }
+}
 export function dealTiles() {
     /* Colors of tiles */
     const tileColors = ['dark-gray', 'dark-tan', 'dark-pink', 'dark-blue', 'dark-yellow'];
@@ -25,10 +37,45 @@ export function dealTiles() {
     for (let i = 0; i < 5; i++) {
         const circle = document.createElement('div');
         circle.classList.add('circle');
-
+        const positions = [];
         for (let j = 0; j < 5; j++) {
             const tile = document.createElement('div');
+            tile.addEventListener('click', handleMoveTile);
+            const tileTooltip = document.createElement('span');
+            tileTooltip.innerHTML = `${allTiles[i * 5 + j]}`;
+            tile.appendChild(tileTooltip);
+
+            const tileSize = 30;
+            const maxPosition = 90;
+            const edgeCircle = 25;
+            const minDistance = 40;
+
+            let x, y;
+            let isValidPosition = false;
+
+            while(!isValidPosition) {
+                // position randomly within circle
+                x = Math.random() * maxPosition + edgeCircle;
+                y = Math.random() * maxPosition + edgeCircle;
+
+                isValidPosition = positions.every(pos => {
+                    const dx = x - pos.x;
+                    const dy = y- pos.y;
+                    const distance = Math.sqrt(dx * dx + dy * dy);
+                    return distance >= minDistance;
+                });
+            }
+            positions.push({ x, y })
             tile.classList.add('tile', allTiles[i * 5 + j]); // Assign a one of the randomly ordered color
+
+            if (j % 2 === 0) {
+                tile.style.top = `${x}px`;
+                tile.style.right = `${y}px`;
+            } else {
+                tile.style.bottom = `${x}px`;
+                tile.style.left = `${y}px`;
+            }
+
             circle.appendChild(tile);
         }
 
@@ -40,3 +87,7 @@ export function dealTiles() {
 export function shuffleTiles () {
     console.log('shuffle tiles')
 }
+
+document.addEventListener('DOMContentLoaded', () => {
+    dealTiles();
+})
