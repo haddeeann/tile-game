432fb16ffa3c18c51d07ef302aa213ef1b8930d8
HaddeeAndd <haddeeann@gmail.com>
2025-02-22 16:49:39 -0500

    can't choose the wrong board

--- pieces.js
+++ pieces.js
@@ -100,6 +100,12 @@ class BoardRow extends HTMLElement {
 
     handleRowSelection(event) {
         event.stopPropagation();
+        const gameBoard = this.getRootNode().host.closest('game-board');
+        const playerContainer = gameBoard.closest('#player1, #player2');
+        if (!playerContainer.classList.contains('current-turn')) {
+            // current player not selected
+            return;
+        }
         // Deselect the previously selected row
         if (selectedRow && selectedRow !== this) {
             this.removeSquareStyles(this.classList.contains('selected'), selectedRow);
